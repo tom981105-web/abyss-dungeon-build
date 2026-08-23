61c062e90b6a4ef6b1bccd08f8a5a792518bede5
ONE <tom981105@gmail.com>
2026-08-23 12:49:40 +0900

    Fix item type namespace

--- src/HybridizerMenu.cs
+++ src/HybridizerMenu.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewValley;
+using StardewValley.ItemTypeDefinitions;
 using StardewValley.Menus;
 
 namespace WatermelonGeneticsCore;
